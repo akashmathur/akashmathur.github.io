f60a355041a038a20b794b2f7f5699f838007449
Akash Mathur <akashmathur97@gmail.com>
2018-08-18 16:04:16 -0400

    gh_1.5 : updated experience page UI, removed old touch-display-hover bug (still need to fix), added projects section with first project page draft complete

--- javascript/script.js
+++ javascript/script.js
@@ -45,31 +45,32 @@ $(window).scroll(function() {
     }
 }).scroll();
 
-//Stack Overflow solution to remove hover on touch devices
-function hasTouch() {
-    return 'ontouchstart' in document.documentElement
-           || navigator.maxTouchPoints > 0
-           || navigator.msMaxTouchPoints > 0;
-}
 
-if (hasTouch()) { // remove all :hover stylesheets
-    try { // prevent exception on browsers not supporting DOM styleSheets properly
-        for (var si in document.styleSheets) {
-            var styleSheet = document.styleSheets[si];
-            if (!styleSheet.rules) continue;
-
-            for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
-                if (!styleSheet.rules[ri].selectorText) continue;
-
-                if (styleSheet.rules[ri].selectorText.match(':hover')) {
-                    styleSheet.deleteRule(ri);
-                }
-            }
-        }
-    } catch (ex) {}
-}
-
-
+////Stack Overflow solution to remove hover on touch devices
+//function hasTouch() {
+//    return 'ontouchstart' in document.documentElement
+//           || navigator.maxTouchPoints > 0
+//           || navigator.msMaxTouchPoints > 0;
+//}
+//
+//if (hasTouch()) { // remove all :hover stylesheets
+//    try { // prevent exception on browsers not supporting DOM styleSheets properly
+//        for (var si in document.styleSheets) {
+//            var styleSheet = document.styleSheets[si];
+//            if (!styleSheet.rules) continue;
+//
+//            for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+//                if (!styleSheet.rules[ri].selectorText) continue;
+//
+//                if (styleSheet.rules[ri].selectorText.match(':hover')) {
+//                    styleSheet.deleteRule(ri);
+//                }
+//            }
+//        }
+//    } catch (ex) {}
+//}
+//
+//
 
 
 
